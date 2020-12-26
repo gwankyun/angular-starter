c6045d0f72c27134970565ddd05866aa577cfbf6
權少 <ljqican@163.com>
2020-12-26 14:52:31 +0800

    先將框架升級到Angular 11

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Observable, of } from 'rxjs'
 
 @Component({
   selector: 'app-root',
@@ -8,34 +7,4 @@ import { Observable, of } from 'rxjs'
 })
 export class AppComponent {
   title = 'angular-starter';
-  disabled = false;
-  value: number = 50;
-  checked: boolean = false;
-  radioValue: string = 'A';
-  selectValue: string = 'A';
-  switchValue: boolean = false;
-  date: Date = null;
-  datePickerMode: string = 'date';
-
-  nums: Observable<number> = of(1, 2, 3);
-
-  items: number[] = [1, 2, 3, 4, 5];
-
-  onChange(date: Date): void {
-    console.log(`date: ${date}`);
-  }
-
-  onOf(): void {
-    this.nums.subscribe({
-      next(x) {
-        console.log(`next: ${x}`);
-      },
-      error(e) {
-        console.error(`error: ${e}`);
-      },
-      complete() {
-        console.log(`complete`)
-      }
-    });
-  }
 }
--- src/app/to-do/to-do.ts
+++
@@ -1,6 +0,0 @@
-export interface ToDo {
-  id: number;
-  content: string;
-  level: number;
-  deleted: boolean;
-}
